3bfd5c0244ad4de2c10009001d6db4718922de5a
Anas Ahmed <imanas559@gmail.com>
2023-06-10 02:54:34 +0530

    Feat: Custom font and background image added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 module.exports = {
   content: ["*"],
   theme: {
-    extend: {},
+    extend: { 
+
+      // New font style added 
+      fontFamily : {
+
+        'Montserrat': ['Montserrat', 'sans-serif']
+
+
+      },
+        
+      // to add different images for different screen size for parrallex effect 
+      backgroundImage: {
+        'image1': "url('images/img12.jpg')",
+        'image2': "url('images/img9.jpg')",
+      }
+
+      
+
+    },
   },
   plugins: [],
 }
