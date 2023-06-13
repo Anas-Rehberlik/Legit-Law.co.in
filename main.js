a1ea60ece5de8d76d6b9a7b29f42d3cfe7cb4e37
Anas Ahmed <imanas559@gmail.com>
2023-06-14 00:44:42 +0530

    Enhancement: Reusable header/footer and restructured navbar code

--- main.js
+++ main.js
@@ -1,37 +1,58 @@
 
 
-// For navbar 
-document.addEventListener("DOMContentLoaded", function() {
-    // your JavaScript code goes here
-    const button = document.querySelector('[data-collapse-toggle]');
-    const menu = document.querySelector('#mobile-menu-2');
-    const navLinks = document.querySelectorAll('#mobile-menu-2 a');
-    const menuIcon = document.querySelector('.menu-icon');
-    const closeIcon = document.querySelector('.close-icon');
-  
-    button.addEventListener('click', function() {
-      const expanded = this.getAttribute('aria-expanded') === 'true' || false;
-      this.setAttribute('aria-expanded', !expanded);
-      menu.classList.toggle('hidden');
-      menuIcon.classList.toggle('hidden');
-      closeIcon.classList.toggle('hidden');
-    });
-  
-    navLinks.forEach(function(link) {
-      link.addEventListener('click', function() {
-        button.setAttribute('aria-expanded', 'false');
-        menu.classList.add('hidden');
-        menuIcon.classList.remove('hidden');
-        closeIcon.classList.add('hidden');
-      });
+// For navbar and header and footer
+function initializeNavbar() {
+  const button = document.querySelector('[data-collapse-toggle]');
+  const menu = document.querySelector('#mobile-menu-2');
+  const navLinks = document.querySelectorAll('#mobile-menu-2 a');
+  const menuIcon = document.querySelector('.menu-icon');
+  const closeIcon = document.querySelector('.close-icon');
+
+  button.addEventListener('click', function() {
+    const expanded = this.getAttribute('aria-expanded') === 'true' || false;
+    this.setAttribute('aria-expanded', !expanded);
+    menu.classList.toggle('hidden');
+    menuIcon.classList.toggle('hidden');
+    closeIcon.classList.toggle('hidden');
+  });
+
+  navLinks.forEach(function(link) {
+    link.addEventListener('click', function() {
+      button.setAttribute('aria-expanded', 'false');
+      menu.classList.add('hidden');
+      menuIcon.classList.remove('hidden');
+      closeIcon.classList.add('hidden');
     });
   });
-  // Code for navbar ends here
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  // Load the header HTML
+  fetch('/header.html')
+    .then(response => response.text())
+    .then(html => {
+      // Insert the header HTML into the document
+      document.getElementById('header-placeholder').innerHTML = html;
+      // Call the function to initialize the navbar
+      initializeNavbar();
+    });
+
+  // Load the footer HTML
+  fetch('/footer.html')
+    .then(response => response.text())
+    .then(html => {
+      // Insert the footer HTML into the document
+      document.getElementById('footer-placeholder').innerHTML = html;
+    });
+});
 
 
   
 
-  
+
+
+
+
 
 
 
@@ -64,4 +85,5 @@ document.addEventListener("DOMContentLoaded", function() {
   observer.observe(videoPlayer);
 });
 
-  
+
+
