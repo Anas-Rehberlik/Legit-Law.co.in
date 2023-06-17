813ec86c9ac720dd9f0009438f48744a65af09d7
Anas Ahmed <imanas559@gmail.com>
2023-06-18 04:13:08 +0530

    Feat: Added vite.config.js for project configuration

---
+++ vite.config.js
@@ -0,0 +1,37 @@
+import path from 'path';
+
+import { defineConfig } from 'vite';
+
+
+export default defineConfig({
+  base: '/',
+  build: {
+    outDir: 'dist',
+
+    rollupOptions: {
+    
+      input: {
+        main: path.resolve(__dirname, './index.html'),
+        about: path.resolve(__dirname, './about.html'),
+        contact: path.resolve(__dirname, './contact.html'),
+        header: path.resolve(__dirname, './header.html'),
+        footer: path.resolve(__dirname, './footer.html'),
+
+
+        // main: './index.html',
+        // about: './about.html',
+        // contact: './contact.html',
+        // header: './header.html',
+        // footer: './footer.html',
+      },
+    },
+    
+  },
+});
+
+
+
+
+
+
+
